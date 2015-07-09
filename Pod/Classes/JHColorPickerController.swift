dd4c2bba73f3169959e1f08d58441096a8547ea5
Julian <julian@appmosphereinc.com>
2015-07-09 18:53:02 -0500

    initial picker xib layout

--- Pod/Classes/JHColorPickerController.swift
+++ Pod/Classes/JHColorPickerController.swift
@@ -10,22 +10,112 @@ import Foundation
 import UIKit
 import ChameleonFramework
 
-public class JHColorPickerController: UIViewController {
+protocol JHPickerControllerDelegate {
+    func colorSelected(color:UIColor, name:String?)
+}
 
-     convenience init() {
+public class JHColorPickerController: UIViewController/*, UICollectionViewDataSource, UICollectionViewDelegate */{
+
+    enum ColorCategory{
+        case Crayola
+        case Pantone
+        case Flat
+        case Custom
+    }
+    
+    @IBOutlet weak var previousColorView:UIView!
+    @IBOutlet weak var selectedColorView:UIView!
+    @IBOutlet weak var categorySelectorSegmentedControl: UISegmentedControl!
+    @IBOutlet weak var swatchView:UIView!
+    @IBOutlet weak var swatchCollectionView:UICollectionView!
+    @IBOutlet weak var customView:UIView!
+    
+    var delegate:JHPickerControllerDelegate?
+    var colorSwatches = Array<Dictionary<String,AnyObject>>()
+    
+    var selectedColorCategory:ColorCategory = .Crayola {
+        didSet {
+            switch selectedColorCategory {
+            case .Crayola:
+                customView.hidden = true
+                swatchView.hidden = false
+            case .Pantone:
+                customView.hidden = true
+                swatchView.hidden = false
+            case .Flat:
+                customView.hidden = true
+                swatchView.hidden = false
+            case .Custom:
+                swatchView.hidden = true
+                customView.hidden = false
+            }
+        }
+    }
+    var selectedColor: UIColor?{
+        didSet{
+            selectedColorView.backgroundColor = selectedColor
+        }
+    }
+    var selectedColorName: String?
+    var previousColor: UIColor? {
+        didSet{
+            previousColorView.backgroundColor = previousColor
+        }
+    }
+    var previousColorName:String?
+    
+    convenience init() {
         let bundle = NSBundle(forClass:JHColorPickerController.self)
         self.init(nibName: "JHColorPickerController.bundle/JHColorPickerController", bundle: bundle)
     }
     
     public override func viewDidLoad() {
+        super.viewDidLoad()
         
-        println("VDL")
+        loadNavButtons()
     }
     
     public override func viewDidAppear(animated: Bool) {
         super.viewDidAppear(animated)
-        println("VDA")
-        self.view.backgroundColor = UIColor.flatSandColor()
+
+
+    }
+    
+    func loadNavButtons() {
+        let leftCancelBtn = UIBarButtonItem(title: "Cancel", style: UIBarButtonItemStyle.Plain, target: self, action: "cancelBtnPressed:")
+        let rightSaveBtn = UIBarButtonItem(title: "Save", style: UIBarButtonItemStyle.Plain, target: self, action: "saveBtnPressed:")
+        self.navigationItem.leftBarButtonItem = leftCancelBtn
+        self.navigationItem.rightBarButtonItem = rightSaveBtn
     }
 
+    func cancelBtnPressed(sender:UIButton) {
+        self.presentingViewController?.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func saveBtnPressed(sender:UIButton) {
+        if let color = selectedColor {
+            delegate?.colorSelected(color, name: selectedColorName)
+        }else {
+            
+        }
+        
+    }
+    
+    @IBAction func colorCategorySegmentedControlValueChanged(sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            selectedColorCategory = .Crayola
+        case 1:
+            selectedColorCategory = .Pantone
+        case 2:
+            selectedColorCategory = .Flat
+        case 3:
+            selectedColorCategory = .Custom
+        default:
+            selectedColorCategory = .Custom
+        }
+    }
+
+    
+    
 }
